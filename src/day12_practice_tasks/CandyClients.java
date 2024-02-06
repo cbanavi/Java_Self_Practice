e0ef09c08ca8f0066dc852250036054609bae19d
cbanavi <cbanavi@gmail.com>
2024-02-06 14:53:12 -0800

    Day 11 & Day 12 tasks incomplete

---
+++ src/day12_practice_tasks/CandyClients.java
@@ -0,0 +1,26 @@
+package day12_practice_tasks;
+
+public class CandyClients {
+}
+
+/*
+4. Create a custom class named Candy with the following specifications:
+
+	Attributes:
+		brand
+		quantity
+		price
+		hasPeanuts
+
+	Ensure encapsulation for all fields.
+		Conditions:
+			The price of candy cannot be set to a negative value.
+			The quantity of candy cannot be set to zero or a negative value.
+
+	Add a constructor that allows the user to set all fields during object creation.
+
+	Actions:
+		toString(): Displays the brand, unit price, and quantity when the object is printed. If the price is zero, display "free" instead of 0.
+
+	Create another class named CandyClients, create multiple candy objects, and test each function of the candy object.
+ */
---
+++ src/day12_practice_tasks/Circle.java
@@ -0,0 +1,24 @@
+package day12_practice_tasks;
+
+public class Circle {
+}
+
+/*
+1. Create a custom class named Circle with the following specifications:
+
+	Attributes:
+		radius
+
+	Ensure encapsulation for the field.
+		Condition:
+			The radius cannot be set to a negative or zero value.
+
+	Add a constructor that allows the user to set the field during object creation.
+
+	Actions:
+		calcArea(): Calculates and returns the area of the circle object.
+		calcPerimeter(): Calculates and returns the perimeter of the circle object.
+		toString(): Displays the radius, area, and perimeter of the circle when the object is printed.
+
+	Create another class named CircleClients, create multiple circle objects, and test each function of the circle object.
+ */
