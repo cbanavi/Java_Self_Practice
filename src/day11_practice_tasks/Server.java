8f49e63ca39d9fb47effb42550021720ca154433
cbanavi <cbanavi@gmail.com>
2024-02-07 22:57:47 -0800

    Day 11 & Day 12 tasks incomplete

---
+++ src/day11_practice_tasks/Server.java
@@ -0,0 +1,23 @@
+package day11_practice_tasks;
+
+public class Server {
+}
+
+/*
+Create a custom class named Server with similar specifications:
+
+	Attributes:
+			name (String)
+			employeeID (int)
+			hourlyRate (double)
+			fullTime (boolean)
+
+	Add A constructor that can set all the fields.
+
+    Actions:
+		makeOrder(): prints chef's name + " is making an order"
+		washDishes(): prints chef's name + " is washing the dishes"
+		toString(): returns a string representation of a Chef, including full-time or part-time status
+
+
+ */
