7e86670b40fa5420b6598bffa2600ee6243cb3b1
cbanavi <cbanavi@gmail.com>
2024-02-10 19:13:18 -0800

    Day 11 & Day 12 & Day 13 tasks incomplete

---
+++ src/day13_practice_tasks/employee_task/Developer.java
@@ -0,0 +1,40 @@
+package day13_practice_tasks.employee_task;
+
+public class Developer {
+}
+
+/*
+1. Create a subclass of Person named "Employee" with the following specifications:
+   Attributes:
+       - employeeId: String
+       - jobTitle: String
+       - salary: double
+       - companyName: String
+
+   Encapsulation:
+       - Same as previous tasks.
+
+   Constructor:
+       - Same as previous tasks.
+
+   Methods:
+       - work(): Displays the employee's job title and name.
+       - toString(): Returns a string representation of the Employee object.
+ 2. Create the following subclasses of Employee:
+   2.1 Tester:
+       - work(): Displays "[jobTitle] [name] is testing".
+
+   2.2 Developer:
+       - Extra Variable:
+           - programmingLanguage: String
+       - work(): Displays "[jobTitle] [name] is coding in [programmingLanguage]".
+
+   2.3 Teacher:
+       - work(): Displays "[name] is teaching".
+
+   2.4 Driver:
+       - work(): Displays "[name] is driving".
+
+   - Add any additional fields or methods as necessary.
+
+ */
---
+++ src/day13_practice_tasks/student_task/GraduateStudent.java
@@ -0,0 +1,55 @@
+package day13_practice_tasks.student_task;
+
+public class GraduateStudent {
+}
+
+/*
+1. Create a class named "Student" with the following specifications:
+   Attributes:
+       - name: String
+       - age: int
+       - gender: String
+
+   Encapsulation:
+       - Same as previous tasks.
+
+   Constructor:
+       - Same as previous tasks.
+
+   Actions:
+       - toString(): Prints the information of the Student object.
+2. Create a subclass named "Student" with the following specifications:
+   Attributes:
+       - studentId: String
+       - fieldOfStudy: String
+       - grade: char
+       - schoolName: String
+
+   Encapsulation:
+       - Same as previous tasks.
+
+   Actions:
+       - study(): Displays a message stating the student's name and school.
+       - toString(): Prints the information of the Student object (including all attributes).
+
+3. Create the following subclasses of the Student class:
+   3.1 GraduateStudent:
+       - Override the study method.
+       - Add any additional fields and methods as necessary.
+
+   3.2 UndergraduateStudent:
+       - Same as GraduateStudent.
+
+   3.3 CydeoStudent:
+       - Additional Attributes:
+           - batchNumber: int
+           - groupNumber: int
+           - programmingLanguage: String
+       - Encapsulation:
+           - Same as previous tasks.
+       - Constructor:
+           - Same as previous tasks.
+       - Actions:
+           - Override the study method.
+           - Override the toString method.
+ */
